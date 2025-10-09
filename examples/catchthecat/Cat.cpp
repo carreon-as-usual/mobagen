ed6baad66bf130cf5815d2bf9f827f43b4cdc533
Caleb Carreon <calebcarreon@gmail.com>
2025-10-09 09:23:19 -0400

    Post encircled algorithm

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -7,47 +7,45 @@ static const int NEIGHBOR_COUNT = 5;
 Point2D Cat::Move(World* world) {
   auto pos = world->getCat();
   auto path = generatePath(world);
-  if(path.empty())
-  {
-    auto rand = Random::Range(0, NEIGHBOR_COUNT);
-    Point2D movePos = pos;
-    int count = 0;
-    while(!world->catCanMoveToPosition(movePos)) {
-      switch (rand) {
-        case 0:
-          movePos = World::NE(pos);
-          break;
-        case 1:
-          movePos = World::NW(pos);
-          break;
-        case 2:
-          movePos = World::E(pos);
-          break;
-        case 3:
-          movePos = World::W(pos);
-          break;
-        case 4:
-          movePos = World::SW(pos);
-          break;
-        case 5:
-          movePos = World::SE(pos);
-          break;
-        default:
-          movePos = pos;
-          break;
-      }
-      rand++;
-      if(rand > NEIGHBOR_COUNT)
-      {
-        rand = 0;
-      }
-      count++;
-      if(count > NEIGHBOR_COUNT + 1)
-      {
-        return pos;
-      }
-    }
-    return movePos;
+  if(!path.empty()) {
+    return path.at(path.size() - 1);
   }
-  return path.at(path.size() - 1);
+  auto neighbors = world->neighbors(pos);
+  int chosenIndex = 0;
+  if(pos.x < 0)
+  {
+    if(pos.y < 0)
+    {
+      chosenIndex = 5;
+    } else if(pos.y == 0)
+    {
+      chosenIndex = 2;
+    } else {
+      chosenIndex = 0;
+    }
+  } else {
+    if(pos.y < 0)
+    {
+      chosenIndex = 4;
+    } else if(pos.y == 0)
+    {
+      chosenIndex = 3;
+    } else {
+      chosenIndex = 1;
+    }
+  }
+  int exitLoop = 0;
+  while(exitLoop < NEIGHBOR_COUNT + 1)
+  {
+    if(world->catcherCanMoveToPosition(neighbors[chosenIndex]) && !world->getContent(neighbors[chosenIndex]))
+    {
+      return neighbors[chosenIndex];
+    }
+    chosenIndex++;
+    exitLoop++;
+    if(chosenIndex > NEIGHBOR_COUNT)
+    {
+      chosenIndex = 0;
+    }
+  }
 }
--- examples/catchthecat/Catcher.cpp
+++ examples/catchthecat/Catcher.cpp
@@ -1,22 +1,51 @@
 #include "Catcher.h"
 #include "World.h"
 
+static const int NEIGHBOR_COUNT = 5;
+
 Point2D Catcher::Move(World* world) {
   std::vector<Point2D> path = generatePath(world);
   if(!path.empty())
   {
   return path[0];
   }
-  Point2D catPos = world->getCat();
+  auto catPos = world->getCat();
   auto neighbors = world->neighbors(catPos);
-  std::vector<Point2D> possiblePoints = std::vector<Point2D>();
-  for(auto neighbor : neighbors)
+  int chosenIndex = 0;
+  if(catPos.x < 0)
   {
-    if(world->isValidPosition(neighbor) && !world->getContent(neighbor))
+    if(catPos.y < 0)
     {
-      possiblePoints.push_back(neighbor);
+      chosenIndex = 5;
+    } else if(catPos.y == 0)
+    {
+      chosenIndex = 2;
+    } else {
+      chosenIndex = 0;
+    }
+  } else {
+    if(catPos.y < 0)
+    {
+      chosenIndex = 4;
+    } else if(catPos.y == 0)
+    {
+      chosenIndex = 3;
+    } else {
+      chosenIndex = 1;
     }
   }
-  int chosenNeighbor = Random::Range(0, possiblePoints.size() - 1);
-  return possiblePoints[chosenNeighbor];
+  int exitLoop = 0;
+  while(exitLoop < NEIGHBOR_COUNT + 1)
+  {
+    if(world->catcherCanMoveToPosition(neighbors[chosenIndex]) && !world->getContent(neighbors[chosenIndex]))
+    {
+      return neighbors[chosenIndex];
+    }
+    chosenIndex++;
+    exitLoop++;
+    if(chosenIndex > NEIGHBOR_COUNT)
+    {
+      chosenIndex = 0;
+    }
+  }
 }
